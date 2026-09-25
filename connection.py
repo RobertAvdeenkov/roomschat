d9a7f891754d86fbba38cf5496619fc8e1317174
RobertAvdeenkov <robertavdeenkov@gmail.com>
2026-09-25 19:35:42 +0300

    time + active users list

--- connection.py
+++ connection.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Connections:
     def __init__(self,name, cons, history, id):
         self.name=name
@@ -7,6 +9,16 @@ class Connections:
 
     async def connect(self, websocket):
         await websocket.accept()
+        active=''
+        for i in self.cons:
+            active+=f'{i.scope['ip']}\n'
+        await websocket.send_text(
+            f'''
+            Добро пожаловать в чат!
+            Список активных пользователей:
+            {active}
+            '''
+        )
         self.cons.append(websocket)
         for i in self.history:
             await websocket.send_text(i)
@@ -21,7 +33,8 @@ class Connections:
     async def receive(self, websocket):
         while True:
             data=await websocket.receive_text()
+            date=datetime.now()
             for i in self.cons:
-                await i.send_text(data)
+                await i.send_text(f'{date.hour}:{date.minute} '+data)
             self.history.append(data)
     
