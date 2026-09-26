fe08eb5465cfa8800eebecd2838dbb1f88a823ec
RobertAvdeenkov <robertavdeenkov@gmail.com>
2026-09-26 14:47:40 +0300

    README + nicknames + nickname input check

--- connection.py
+++ connection.py
@@ -1,41 +1,52 @@
 from datetime import datetime
 
 class Connections:
-    def __init__(self,name, cons, history, id):
+    def __init__(self,name, cons, history, id,creator):
         self.name=name
         self.id=int(id)
+        self.creator=creator
         self.cons=list()
+        self.nicklist=list()
         self.history=list()
 
-    async def connect(self, websocket):
+    async def connect(self, websocket, nickname):
         await websocket.accept()
         active=''
-        for i in self.cons:
-            active+=f'{i.scope['client'][0]}<br>'
+        for i in self.nicklist:
+            active+=f'{i}<br>'
         await websocket.send_text(
             f'''
-            Добро пожаловать в чат!<br>
+            Добро пожаловать в чат, {nickname}!<br>
             Список активных пользователей:
-            {active}
+            {active if active else 'Пользователей пока нет!'}
             '''
         )
         self.cons.append(websocket)
+        self.nicklist.append(nickname)
         for i in self.history:
             await websocket.send_text(i)
 
-    async def disconnect(self, websocket):
+    async def disconnect(self, websocket, nickname):
         try:
             self.cons.remove(websocket)
+            self.nicklist.remove(nickname)
         finally:
             for i in self.cons:
-                await i.send_text(f'Участник вышел')
+                await i.send_text(f'{nickname} вышел')
+            self.history.append(f'{nickname} вышел')
 
-    async def receive(self, websocket):
+    async def receive(self, websocket, nickname):
         while True:
             data=await websocket.receive_text()
             date=datetime.now()
-            data=f'{date.hour}:{date.minute} '+data
+            data=f'{nickname}<br>'+data
+            txt=''
+            for index,i in enumerate(data):
+                if index%120==0 and index!=0:
+                    txt+='<br>'+i
+                else: txt+=i
+            txt+=f'<br>{date.hour}:{date.minute if date.minute>9 else f'0{date.minute}'}'
             for i in self.cons:
-                await i.send_text(data)
-            self.history.append(data)
+                await i.send_text(txt)
+            self.history.append(txt)
     
--- tasks.py
+++ tasks.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter,Query,WebSocket,WebSocketException
+from fastapi import APIRouter,Query,WebSocket,WebSocketException,Cookie,HTTPException
 from connection import Connections
 from fastapi.responses import Response,RedirectResponse,FileResponse
 import sys
+from urllib.parse import unquote
 
 id=0
 
@@ -10,35 +11,37 @@ rooms=[]
 router=APIRouter()
 
 @router.get('/')
-async def mainpage():
+async def mainpage( nickname=Cookie()):
     return FileResponse('templates/mainpage.html')
 
 @router.post('/append')
-async def add_room(name=Query()):
+async def add_room(name=Query(), nickname=Cookie()):
     global id
-    target=Connections(name=name,cons=[], history=[], id=id)
+    if len(name)>50:
+        raise HTTPException(400,'Название должно быть меньше 50 символов!')
+    target=Connections(name=name,cons=[], history=[], id=id, creator=nickname)
     id+=1
     rooms.append(target)
     return {'status':'success'}
 
 @router.get('/rooms')
 async def roomsSHOW():
-    return {'rooms': [{'id': room.id, 'name': room.name} for room in rooms]}
+    return {'rooms': [{'id': room.id, 'name': f'{room.name} от владельца {room.creator}'} for room in rooms]}
 
 @router.get('/room')
-async def room(id:int):
+async def room(id:int, nickname=Cookie()):
     return FileResponse('templates/room.html')
 
 @router.websocket('/ws/{id}')
-async def ws(websocket:WebSocket, id:int):
+async def ws(websocket:WebSocket, id:int, nickname=Cookie()):
+    name=nickname
     for i in rooms:
         if i.id==id:
-            print(websocket.state)
-            await i.connect(websocket)
+            await i.connect(websocket,name)
             try:
-                await i.receive(websocket)
+                await i.receive(websocket,name)
             except:
-                await i.disconnect(websocket)
+                await i.disconnect(websocket,name)
                 return 
 
 
