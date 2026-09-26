bae2a12b875fa7df34a72670aa85209be40448b5
RobertAvdeenkov <robertavdeenkov@gmail.com>
2026-09-26 18:37:38 +0300

    Join message + time in join\leave messages

--- connection.py
+++ connection.py
@@ -11,9 +11,18 @@ class Connections:
 
     async def connect(self, websocket, nickname):
         await websocket.accept()
+        date=datetime.now()
+        jointext=f'{date.hour}:{date.minute if date.minute>9 else f'0{date.minute}'} {nickname} присоединился'
+        for i in self.cons:
+            await i.send_text(jointext)
+        self.history.append(jointext)
         active=''
         for i in self.nicklist:
-            active+=f'{i}<br>'
+            active+=f'<button style="padding: 0px; background-color: transparent;" onclick="appeal(`{i}`)">{i}</button><br>'
+        self.cons.append(websocket)
+        self.nicklist.append(nickname)
+        for i in self.history:
+            await websocket.send_text(i)
         await websocket.send_text(
             f'''
             Добро пожаловать в чат, {nickname}!<br>
@@ -21,31 +30,28 @@ class Connections:
             {active if active else 'Пользователей пока нет!'}
             '''
         )
-        self.cons.append(websocket)
-        self.nicklist.append(nickname)
-        for i in self.history:
-            await websocket.send_text(i)
 
     async def disconnect(self, websocket, nickname):
+        date=datetime.now()
         try:
             self.cons.remove(websocket)
             self.nicklist.remove(nickname)
         finally:
             for i in self.cons:
-                await i.send_text(f'{nickname} вышел')
-            self.history.append(f'{nickname} вышел')
+                await i.send_text(f'{date.hour}:{date.minute if date.minute>9 else f'0{date.minute}'} {nickname} вышел')
+            self.history.append(f'{date.hour}:{date.minute if date.minute>9 else f'0{date.minute}'} {nickname} вышел')
 
     async def receive(self, websocket, nickname):
         while True:
             data=await websocket.receive_text()
             date=datetime.now()
-            data=f'{nickname}<br>'+data
             txt=''
             for index,i in enumerate(data):
-                if index%120==0 and index!=0:
+                if index%50==0 and index!=0:
                     txt+='<br>'+i
                 else: txt+=i
             txt+=f'<br>{date.hour}:{date.minute if date.minute>9 else f'0{date.minute}'}'
+            txt=f'<button style="padding: 0px; background-color: transparent;" onclick="appeal(`{nickname}`)">{nickname}</button><br>'+txt
             for i in self.cons:
                 await i.send_text(txt)
             self.history.append(txt)
